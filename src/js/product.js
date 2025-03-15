9c0fa6c42f5cc3413fec401625eba154ee9ae159
Saintrick-FM <francymalonga8@gmail.com>
2025-03-15 10:18:41 +0100

    fix(addProduct): add product to cart fix, localstorage now adds products succesfully.

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+/* eslint-disable quotes */
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || [];
+  // check if cart items is an array
+  if (Array.isArray(cartItems)) {
+    cartItems.push(product);
+    setLocalStorage("so-cart", cartItems);
+  } else {
+    setLocalStorage("so-cart", [product]); // if it's not an array, create one with the product and save it
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
