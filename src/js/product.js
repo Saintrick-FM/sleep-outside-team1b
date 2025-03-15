0f164b285a49ab224abc53979184d6acb3e61636
Saintrick-FM <francymalonga8@gmail.com>
2025-03-15 11:42:11 +0100

    feat(dynamic-pruduct-detail-page): Done with W02 Team Activity: Dynamic Product Detail Page

--- src/js/product.js
+++ src/js/product.js
@@ -1,26 +1,9 @@
-/* eslint-disable quotes */
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  // check if cart items is an array
-  if (Array.isArray(cartItems)) {
-    cartItems.push(product);
-    setLocalStorage("so-cart", cartItems);
-  } else {
-    setLocalStorage("so-cart", [product]); // if it's not an array, create one with the product and save it
-  }
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
